b503d5cababf533a2c451a782913c4b582ab1b4e
karpss <ola.fam10@gmail.com>
2022-01-12 08:51:14 +0100

    started work to implement graph. fectch graph data from api done

--- src/components/Graph/Graph.js
+++ src/components/Graph/Graph.js
@@ -1,9 +1,10 @@
 /* eslint-disable */
 import React from 'react';
+import styles from './Graph.module.css';
 
 const Graph = () => {
     return (
-        <div>
+        <div className={styles.graphgrid}>
             Graph
         </div>
     )
